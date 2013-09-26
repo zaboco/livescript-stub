506a2b894ff2b95ac2f414f83029e99d80ed8d6b
Bogdan Zaharia <zaboco@gmail.com>
2013-09-26 18:35:15 +0300

    grunt: add lineending plugin + verbosity fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,22 +4,23 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-livescript");
+  grunt.loadNpmTasks("grunt-lineending");
+  grunt.loadNpmTasks("grunt-verbosity");
 
   grunt.initConfig({
     clean: {
       lib: 'lib/',
-      spec: ['src/*.js', 'spec/*.js']
+      tmp: ['src/*.js', 'spec/*.js']
     },
-
     copy: {
       lib: {
         files: [{ expand: true, cwd: 'src/', src: ['*.js'], dest: 'lib/'}]
       }
     },
-
     livescript: {
       options: {
-        bare: true
+        bare: true,
+        prelude: true
       },
       src: {
         files: [{ expand: true, src: ['src/*.ls'], dest: '.', ext: '.js'}]
@@ -42,6 +43,22 @@ module.exports = function(grunt) {
         src: "spec/*.js"
       }
     },
+    lineending: {
+      all: {
+        files: [{
+          expand: true,
+          src: ['**/*', '!node_modules/**/*', '!tmp/**/*'],
+          filter: 'isFile',
+          dest: '.'
+        }]
+      }
+    },
+    verbosity: {
+      all: {
+        options: { mode: 'dot' },
+        tasks: ['lineending', 'livescript', 'clean']
+      }
+    },
     pkg: require('./package.json')
   });
 
@@ -57,7 +74,9 @@ module.exports = function(grunt) {
     });
   });
 
-  grunt.task.registerTask('dist', ['clean:lib', 'test', 'livescript:src', 'copy:lib', 'clean:spec']);
-  grunt.task.registerTask('test', ['clean:spec', 'livescript', 'vows', 'clean:spec']);
+  grunt.task.registerTask('build', ['verbosity', 'lineending', 'livescript']);
+  grunt.task.registerTask('dist', ['clean:lib', 'test', 'livescript:src', 'copy:lib', 'clean:tmp']);
+  grunt.task.registerTask('test', ['clean:tmp', 'build', 'vows', 'clean:tmp']);
+  grunt.task.registerTask('w', ['clean:tmp', 'watch']);
   grunt.task.registerTask('default', ['dist']);
 }
